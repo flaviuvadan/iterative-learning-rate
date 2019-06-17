ba64f9077c298036d1d83a833dc6f93a8bef29b2
FlaviuVadan <flaviuvadan@gmail.com>
2019-06-16 18:35:54 -0600

    Add package init and main file

---
+++ iterative_learning_rate/__init__.py
@@ -0,0 +1,4 @@
+""" Export file """
+
+from .iterative_learning_rate import IterativeLearningRate
+from .plot import Plot
---
+++ main.py
@@ -0,0 +1,10 @@
+""" Main file to run the iterative learning rate package """
+
+from iterative_learning_rate import IterativeLearningRate, Plot
+
+
+def main():
+    pass
+
+
+main()
